de99cc3b8a2074123f3e8664b5430dc08d491e13
jamieroszel <jamie.s.roszel@gmail.com>
2021-03-14 22:38:17 -0400

    set up server.js

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
-// Load express
+// require our modules and declare our settings variables 
 const express = require('express');
+const port = 3000;
 
 // Create our express app
 const app = express();
 
-// Define a "root" route directly on app
-// Tomorrow, we'll use best practice routing
-app.get('/', function (req, res) {
-  res.send('<h1>Hello World!</h1>');
-});
+// configure server settings with app.set()
+app.set('view engine', 'ejs');
 
-// Tell the app to listen on port 3000
-// for HTTP requests from clients
-app.listen(3000, function () {
-  console.log('Listening on port 3000');
+// mount our middleware with app.use()
+
+
+
+// mount our routes
+
+
+
+// tell the app to listen
+app.listen(port, function () {
+    console.log(`Express is listening on port:${port}`);
 });
