e6c9e55123130b1608556f5cc06c660284887cd4
jamieroszel <jamie.s.roszel@gmail.com>
2021-03-16 13:49:37 -0400

    final commit. couldn't get home button to work, but all else functional

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
 })
 // morgan for logging
 app.use(morgan("dev"))
-// urlecoded into order to receive data from forms
+// urlencoded into order to receive data from forms
 app.use(express.urlencoded({extended: false}))
 // serves the public folder as static
 app.use(express.static("public"))
